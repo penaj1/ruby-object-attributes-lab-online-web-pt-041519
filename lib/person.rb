a01aef86ee45a35e050427242e211ece8c1e76a5
Janice Pena <jipena94@gmail.com>
2019-05-08 00:54:14 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+def initialize
+end
+
+def name=(my_name)
+  @name = my_name
+end
+
+def name
+  @name
+end
+
+def job=(job_type)
+  @job = job_type
+end
+
+def job
+  @job
+end
+end
